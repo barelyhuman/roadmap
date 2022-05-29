baa5c56e556b4eb05f675c87b8c01594c8a67d08
Reaper <ahoy@barelyhuman.dev>
2022-05-29 20:58:13 +0530

    base setup

---
+++ src/pages/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { NotionRenderer, getPageBlocks } from 'vue3-notion'
+
+const blockMap = ref()
+
+onMounted(async () => {
+  blockMap.value = await getPageBlocks(import.meta.env.VITE_PAGE_ID)
+})
+</script>
+
+<template>
+  <div v-if="!blockMap" class="fixed top-5 right-5 loader" />
+
+  <NotionRenderer v-if="blockMap" :block-map="blockMap" full-page />
+</template>
+
---
+++ unocss.config.ts
@@ -0,0 +1,27 @@
+import {
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetUno,
+  presetWebFonts,
+} from 'unocss'
+
+export default defineConfig({
+  shortcuts: [
+    ['loader', 'h-4 w-4 rounded-full border-2 border-r-gray-700 border-t-gray-700 border-white animate-spin'],
+  ],
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetIcons({
+      scale: 1.2,
+      warn: true,
+    }),
+    presetWebFonts({
+      provider: 'google',
+      fonts: {
+        sans: 'Inter',
+      },
+    }),
+  ],
+})
